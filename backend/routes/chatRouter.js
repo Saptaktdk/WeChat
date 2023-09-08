7853d1160fc2192da673c614af9e53448e291380
Saptaktdk <dassaptak0@gmail.com>
2023-09-08 16:04:32 +0530

    :fire: Implemented chat APIs

--- backend/routes/chatRouter.js
+++ backend/routes/chatRouter.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const { middleWare } = require('../middleware/authMiddleware');
-const { accessChat } = require("../controllers/chatController");
+const { 
+    accessChat, 
+    getChats, 
+    createGroupChat, 
+    renameGroup, 
+    addToGroup,
+    removeFromGroup
+ } = require("../controllers/chatController");
 
 
 const router = express.Router();
 
 router.route('/').post(middleWare, accessChat);
-// router.route('/').get(middleWare, getChats);
-// router.route('/group').post(middleWare, createGroupChat);
-// router.route('/rename').put(middleWare, renameGroup);
-// router.route('/groupremove').put(middleWare, removeFromGroup);
-// router.route('/groupadd').put(middleWare, addToGroup);
+router.route('/').get(middleWare, getChats);
+router.route('/group').post(middleWare, createGroupChat);
+router.route('/rename').put(middleWare, renameGroup);
+router.route('/groupadd').put(middleWare, addToGroup);
+router.route('/groupremove').put(middleWare, removeFromGroup);
+
 
 module.exports = router; 
